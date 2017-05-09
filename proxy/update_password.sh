f51917c7a46c9a095f5a01fa7b69113a5fc7b6b3
root <root@packer-debian-8-amd64.droplet.local>
2017-05-09 07:27:52 +0000

    added password updating scripts

--- proxy/update_password.sh
+++ proxy/update_password.sh
@@ -6,6 +6,7 @@ LINE=$2
 ERR=log/passord_update
 
 PASS=/etc/shadow
+AUTH=user.list
 
 input_error () {
 	echo "Input error: $(date): \"$NAME\": \"$LINE\"" >> $ERR
@@ -25,12 +26,18 @@ if [ -z $LINE ] ; then
 	input_error
 fi
 
-if [ -z $(cat $PASS | grep ^$NAME) ] ; then
+if [ -z $(cat $PASS | grep ^$NAME: ) ] ; then
 	lookup_error
 fi
 
+if [ -z $(cat $AUTH | grep ^$NAME$ ) ] ; then
+	lookup_error
+fi
+
+REMAINDER=$(cat $PASS | grep ^$NAME: | cut -d: -f3- )
+
 cat $PASS | grep -v ^$NAME | cat > .tempupdatepass
 mv .tempupdatepass $PASS
-echo $LINE >> $PASS 
+echo "$NAME:$LINE:$REMAINDER" >> $PASS 
 
 exit 0
